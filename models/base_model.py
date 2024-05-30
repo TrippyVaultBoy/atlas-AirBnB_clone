1545586a61cdcb0dc39cde05531e8fde45bef76b
Tanner Saint <tsaint2004@gmail.com>
2024-05-30 23:34:30 +0000

    added now method

--- models/base_model.py
+++ models/base_model.py
@@ -5,8 +5,8 @@ from datetime import datetime
 class BaseModel:
     def __init__(self, id=None, created_at=None, updated_at=None):
         self.id = str(uuid.uuid4)
-        self.created_at = created_at
-        self.updated_at = created_at
+        self.created_at = datetime.datetime.now()
+        self.updated_at = datetime.datetime.now()
 
     def save(self):
         """updates the public instance attribute
