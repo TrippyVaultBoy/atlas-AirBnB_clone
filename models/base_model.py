b16dce82d09e6105690fd1f3630318462d2efe81
Tanner Saint <tsaint2004@gmail.com>
2024-05-30 23:16:08 +0000

    added attributes

---
+++ models/base_model.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+import uuid
+from datetime import date, datetime
+
+class Basemodel():
+    def __init__(self, id, created_at, updated_at):
+        self.id = str(uuid.uuid4)
+        self.created_at = datetime.datetime.utcnow()
+        self.updated_at = datetime.datetime.utcnow()
+
+    def save(self):
+        """updates the public instance attribute
+        updated_at with the current datetime"""
+        pass
+
+    def to_dict(self):
+        """returns a dictionary containing all
+        keys/values of __dict__ of the instance:"""
+        pass
