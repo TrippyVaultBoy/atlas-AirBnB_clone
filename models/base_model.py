02a09809b6ce79720775fdc8a98bf6874c76f429
Tanner Saint <tsaint2004@gmail.com>
2024-05-30 23:17:00 +0000

    changed class name

--- models/base_model.py
+++ models/base_model.py
@@ -2,7 +2,7 @@
 import uuid
 from datetime import date, datetime
 
-class Basemodel():
+class BaseModel():
     def __init__(self, id, created_at, updated_at):
         self.id = str(uuid.uuid4)
         self.created_at = datetime.datetime.utcnow()
