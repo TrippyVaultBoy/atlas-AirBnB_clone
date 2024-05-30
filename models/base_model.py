2fbc6c202ade1748ce40077bc91b08845ed921c7
Tanner Saint <tsaint2004@gmail.com>
2024-05-30 23:41:08 +0000

    changed uuid creation

--- models/base_model.py
+++ models/base_model.py
@@ -4,7 +4,7 @@ import datetime
 
 class BaseModel:
     def __init__(self, id=None, created_at=None, updated_at=None):
-        self.id = str(uuid.uuid4)
+        self.id = str(uuid.uuid4())
         self.created_at = datetime.datetime.now()
         self.updated_at = datetime.datetime.now()
 
