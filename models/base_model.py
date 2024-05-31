bae8caae155fbee1f2f0b20498a102a693053c8a
Tanner Saint <tsaint2004@gmail.com>
2024-05-31 00:43:38 +0000

    reverted change

--- models/base_model.py
+++ models/base_model.py
@@ -14,7 +14,7 @@ class BaseModel:
     def save(self):
         """updates the public instance attribute
         updated_at with the current datetime"""
-         return self.updated_at = datetime.datetime.now()
+        self.updated_at = datetime.datetime.now()
 
     def to_dict(self):
         """returns a dictionary containing all
