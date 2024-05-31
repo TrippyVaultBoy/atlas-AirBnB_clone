77da68726ae299c677592ba03ccd303fcb67145a
Tanner Saint <tsaint2004@gmail.com>
2024-05-31 00:20:43 +0000

    added quotes

--- models/base_model.py
+++ models/base_model.py
@@ -19,7 +19,7 @@ class BaseModel:
     def to_dict(self):
         """returns a dictionary containing all
         keys/values of __dict__ of the instance:"""
-        self.__dict__[__class__] = self.__class__.__name__
-        self.__dict__[created_at] = self.created_at.isoformat()
-        self.__dict__[updated_at] = self.updated_at.isoformat()
+        self.__dict__["__class__"] = self.__class__.__name__
+        self.__dict__["created_at"] = self.created_at.isoformat()
+        self.__dict__["updated_at"] = self.updated_at.isoformat()
         return self.__dict__
