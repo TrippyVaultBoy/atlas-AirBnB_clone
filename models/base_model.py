390614c170e61b59f10ec9861a7b27e157128850
Tanner Saint <tsaint2004@gmail.com>
2024-05-31 01:39:35 +0000

    changed init declarations

--- models/base_model.py
+++ models/base_model.py
@@ -4,6 +4,10 @@ import datetime
 
 class BaseModel:
     def __init__(self, id=None, created_at=None, updated_at=None, *args, **kwargs):
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.datetime.now()
+        self.updated_at = datetime.datetime.now()
+        
         if kwargs is not None:
             for key in kwargs:
                 if key == "id":
@@ -12,10 +16,6 @@ class BaseModel:
                     self.created_at = kwargs[key]
                 elif key == "updated_at":
                     self.updated_at = kwargs[key]
-        else:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.datetime.now()
-            self.updated_at = datetime.datetime.now()
 
     def __str__(self):
         return ("[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__))
