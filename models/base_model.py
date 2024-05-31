922f6db9f6340a56fbae11b4c28945411a7aa5b9
jaylenperez <jaylenestella24@gmail.com>
2024-05-31 17:07:15 -0500

    removed utc

--- models/base_model.py
+++ models/base_model.py
@@ -5,8 +5,8 @@ import datetime
 class BaseModel:
     def __init__(self, *args, **kwargs):
         self.id = str(uuid.uuid4())
-        self.created_at = datetime.datetime.utcnow()
-        self.updated_at = datetime.datetime.utcnow()
+        self.created_at = datetime.datetime.now()
+        self.updated_at = datetime.datetime.now()
         
         if kwargs is not None:
             for key, value in kwargs.items():
