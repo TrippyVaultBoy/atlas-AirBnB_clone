3480fd6e6534d7663833eca289188e963e753d99
Tanner Saint <tsaint2004@gmail.com>
2024-05-31 02:22:00 +0000

    chnaged key check

--- models/base_model.py
+++ models/base_model.py
@@ -3,19 +3,17 @@ import uuid
 import datetime
 
 class BaseModel:
-    def __init__(self, id=None, created_at=None, updated_at=None, *args, **kwargs):
+    def __init__(self, *args, **kwargs):
         self.id = str(uuid.uuid4())
         self.created_at = datetime.datetime.now()
         self.updated_at = datetime.datetime.now()
         
         if kwargs is not None:
             for key, value in kwargs.items():
-                if key == "id":
+                if key != "__class__":
                     setattr(self, key, value)
-                elif key == "created_at":
-                    value = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
-                elif key == "updated_at":
-                    value = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
+                elif key == "created_at" or key == "updated_at":
+                    setattr(self, key, datetime.datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f'))
 
     def __str__(self):
         return ("[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__))
@@ -28,7 +26,8 @@ class BaseModel:
     def to_dict(self):
         """returns a dictionary containing all
         keys/values of __dict__ of the instance:"""
-        self.__dict__["__class__"] = self.__class__.__name__
-        self.__dict__["created_at"] = self.created_at.isoformat()
-        self.__dict__["updated_at"] = self.updated_at.isoformat()
-        return self.__dict__
+        my_dict = self.__dict__.copy()
+        my_dict["__class__"] = self.__class__.__name__
+        my_dict["created_at"] = self.created_at.isoformat()
+        my_dict["updated_at"] = self.updated_at.isoformat()
+        return my_dict
