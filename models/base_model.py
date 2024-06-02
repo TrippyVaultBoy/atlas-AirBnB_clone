71deef9ab084ff26cfcbe25811204ecff1e7661e
Tanner Saint <tsaint2004@gmail.com>
2024-06-02 23:23:03 +0000

    added calls to save and new in base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -1,19 +1,21 @@
 #!/usr/bin/python3
 import uuid
 import datetime
+from models.engine.file_storage import storage
 
 class BaseModel:
     def __init__(self, *args, **kwargs):
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.datetime.now()
-        self.updated_at = datetime.datetime.now()
-        
         if kwargs is not None:
             for key, value in kwargs.items():
                 if key != "__class__":
                     setattr(self, key, value)
                 if key == "created_at" or key == "updated_at":
                     setattr(self, key, datetime.datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f'))
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.datetime.now()
+            self.updated_at = datetime.datetime.now()
+            storage.new(self)
 
     def __str__(self):
         """
@@ -26,6 +28,7 @@ class BaseModel:
         """updates the public instance attribute
         updated_at with the current datetime"""
         self.updated_at = datetime.datetime.now()
+        storage.save()
 
     def to_dict(self):
         """returns a dictionary containing all
