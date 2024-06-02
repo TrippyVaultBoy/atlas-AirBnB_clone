f6f54c1e698622e3d13ac3df3911ef9ca9e2f554
Tanner Saint <tsaint2004@gmail.com>
2024-06-02 23:34:56 +0000

    changed init order

--- models/base_model.py
+++ models/base_model.py
@@ -5,16 +5,16 @@ import models
 
 class BaseModel:
     def __init__(self, *args, **kwargs):
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.datetime.now()
+        self.updated_at = datetime.datetime.now()
+        
         if kwargs is not None:
             for key, value in kwargs.items():
                 if key == "created_at" or key == "updated_at":
                     setattr(self, key, datetime.datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f'))
                 if key != "__class__":
                     setattr(self, key, value)
-        else:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.datetime.now()
-            self.updated_at = datetime.datetime.now()
 
     def __str__(self):
         """
