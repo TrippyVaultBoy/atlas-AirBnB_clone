0087176bac80d74068f99932fe5feb4b753b4d2f
Tanner Saint <tsaint2004@gmail.com>
2024-06-02 23:33:04 +0000

    swapped if statements in init

--- models/base_model.py
+++ models/base_model.py
@@ -7,10 +7,10 @@ class BaseModel:
     def __init__(self, *args, **kwargs):
         if kwargs is not None:
             for key, value in kwargs.items():
-                if key != "__class__":
-                    setattr(self, key, value)
                 if key == "created_at" or key == "updated_at":
                     setattr(self, key, datetime.datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f'))
+                if key != "__class__":
+                    setattr(self, key, value)
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.datetime.now()
