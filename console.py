40611a1429e5ba22002a4b7c6bda2d14b8145a00
Tanner Saint <tsaint2004@gmail.com>
2024-06-02 23:58:59 +0000

    documanted console.py

--- console.py
+++ console.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
-
 import cmd
 
+
 class HBNBCommand(cmd.Cmd):
+    """
+    Entry point of the command interpreter
+    """
     prompt = "(hbnb)"
 
     def do_quit(self, arg):
