f6851bd9c17bce356a801907c4580e5326fdfa41
Tanner Saint <tsaint2004@gmail.com>
2024-06-02 23:30:40 +0000

    removed line from __init__

--- models/base_model.py
+++ models/base_model.py
@@ -15,7 +15,6 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.datetime.now()
             self.updated_at = datetime.datetime.now()
-            storage.new(self)
 
     def __str__(self):
         """
