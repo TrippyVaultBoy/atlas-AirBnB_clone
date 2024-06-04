137dc460136565b803a238f305e17171458fac11
Tanner Saint <tsaint2004@gmail.com>
2024-06-04 00:10:36 +0000

    finished tests for User class

--- tests/test_user.py
+++ tests/test_user.py
@@ -14,3 +14,21 @@ class TestUser(unittest.TestCase):
         self.assertEqual(self.user.email, "")
         self.user.email = "test@example.com"
         self.assertEqual(self.user.email, "test@example.com")
+
+    def test_password(self):
+        self.user = User()
+        self.assertEqual(self.user.password, "")
+        self.user.password = "12345"
+        self.assertEqual(self.user.password, "12345")
+
+    def test_first_name(self):
+        self.user = User()
+        self.assertEqual(self.user.first_name, "")
+        self.user.first_name = "John"
+        self.assertEqual(self.user.first_name, "John")
+
+    def test_last_name(self):
+        self.user = User()
+        self.assertEqual(self.user.last_name, "")
+        self.user.last_name = "Smith"
+        self.assertEqual(self.user.last_name, "Smith")
