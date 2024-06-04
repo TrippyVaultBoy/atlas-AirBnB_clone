a7c7f35c5c0e2b43a84bb4fb5a79f62ccf925fe4
Tanner Saint <tsaint2004@gmail.com>
2024-06-04 22:51:19 +0000

    added test_all test_new to test_file_storage.py

---
+++ tests/test_models/test_file_storage.py
@@ -0,0 +1,34 @@
+from models.base_model import BaseModel
+from models.user import User
+from models.state import State
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.amenity import Amenity
+from models.engine.file_storage import FileStorage
+import unittest
+from unittest.mock import patch
+import json
+import os
+
+
+class TestFileStorage(unittest.TestCase):
+    def test_all(self):
+        self.storage = FileStorage()
+        test_dict = self.storage.all()
+        self.assertEqual(type(test_dict), dict)
+        self.assertIs(test_dict, self.storage._FileStorage__objects)
+
+    def test_new(self):
+        self.storage = FileStorage()
+        self.obj = BaseModel()
+        self.storage.new(obj)
+        key = f"BaseModel.{obj.id}"
+        self.assertIn(key, self.storage._FileStorage__objects)
+        self.assertEqual(self.storage._FileStorage__objects[key], obj)
+
+    def test_save(self):
+        pass
+
+    def test_reload(self):
+        pass
